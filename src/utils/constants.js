aa4e31c42bda881fe3d9298238b07436fba1153b
Golden1636 <okeamagolden@yahoo.com>
2023-03-07 22:27:08 +0100

    added more categories to the constants.js file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,16 +11,20 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import WavesIcon from '@mui/icons-material/Waves';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 // A category array that we are going to use on our sidebar
 export const categories = [
   { name: 'New', icon: <HomeIcon /> },
-  { name: 'JS Mastery', icon: <CodeIcon /> },
+  { name: 'Radiography', icon: <AcUnitIcon /> },
+  { name: 'Ultrasound', icon: <WavesIcon /> },
   { name: 'Coding', icon: <CodeIcon /> },
   { name: 'ReactJS', icon: <CodeIcon /> },
   { name: 'NextJS', icon: <CodeIcon /> },
+  { name: 'JS Mastery', icon: <CodeIcon /> },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon /> },
   { name: 'Podcast', icon: <GraphicEqIcon /> },
